853ef1e57d22af2379e929b79c2a1cff0cf1909f
Andy Hood <andythehood@gmail.com>
2025-11-05 15:55:04 +1100

    update core app

--- web/core/src/App.tsx
+++ web/core/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Core App v2</h1>
+      <h1>Core App v3 </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
