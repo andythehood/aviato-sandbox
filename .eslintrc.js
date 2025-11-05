34bae21f358be9bdd1159a68148293eaa3b00bb4
Andy Hood <andythehood@gmail.com>
2025-11-05 16:22:52 +1100

    update eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./web/*/tsconfig.json'], // or wherever your tsconfigs are
+    tsconfigRootDir: __dirname,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+};
