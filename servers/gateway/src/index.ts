35bae68781be6c3498f92c92ce264b4122b3f761
Andy Hood <andythehood@gmail.com>
2025-11-06 14:18:11 +1100

    update gateway service

--- servers/gateway/src/index.ts
+++ servers/gateway/src/index.ts
@@ -11,7 +11,7 @@ app.use('/health', healthRouter);
 
 
 app.get('/', (req, res) => {
-res.json({ message: 'Hello from Express + TypeScript on Cloud Run!' });
+res.json({ message: 'Hello from Gateway Service on Cloud Run!' });
 });
 
 
