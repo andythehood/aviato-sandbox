6705b2739c2d1e5ce323d34113aaf1395c6e8ec9
Andy Hood <andythehood@gmail.com>
2025-11-06 20:48:30 +1100

    updates

--- servers/gateway/src/index.ts
+++ servers/gateway/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { router as healthRouter } from './routes/health';
+import fetch from "node-fetch";
 
 
 const app = express();
@@ -10,13 +11,43 @@ app.use(express.json());
 app.use('/health', healthRouter);
 
 
+
+
+
+app.get('/sites', async (req, res) => {
+
+  const targetUrl = "http://sites.us-central1.run.internal";
+
+  const resp = await fetch(targetUrl, {});
+
+  const data = await resp.json();
+
+  console.log(data);
+
+  res.json({ message: 'Calling Remote Sites Service on Cloud Run', data });
+});
+
+app.get('/users', async (req, res) => {
+
+  const targetUrl = "http://users.us-central1.run.internal";
+
+  const resp = await fetch(targetUrl, {});
+
+  const data = await resp.json();
+
+  console.log(data);
+
+  res.json({ message: 'Calling Remote Users Service on Cloud Run', data });
+});
+
+
 app.get('/', (req, res) => {
-res.json({ message: 'Gateway Service on Cloud Run' });
+  res.json({ message: 'Gateway Service on Cloud Run' });
 });
 
 
 app.listen(port, () => {
-console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
 
 
