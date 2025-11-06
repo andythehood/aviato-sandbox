db1657d75e3954ced898021e1369d186020a0e18
Andy Hood <andythehood@gmail.com>
2025-11-06 13:30:14 +1100

    add gateway server

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['./web/*/tsconfig.json'], // or wherever your tsconfigs are
+    project: ['./web/*/tsconfig.json', './servers/*/tsconfig.json'], // or wherever your tsconfigs are
     tsconfigRootDir: __dirname,
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
---
+++ servers/gateway/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { router as healthRouter } from './routes/health';
+
+
+const app = express();
+const port = Number(process.env.PORT || 8080);
+
+
+app.use(express.json());
+app.use('/health', healthRouter);
+
+
+app.get('/', (req, res) => {
+res.json({ message: 'Hello from Express + TypeScript on Cloud Run!' });
+});
+
+
+app.listen(port, () => {
+console.log(`Server listening on port ${port}`);
+});
+
+
+export default app;
