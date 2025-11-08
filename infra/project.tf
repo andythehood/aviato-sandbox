89634ac52e7030b06d70ec598add9d91f1a39050
Andy Hood <andythehood@gmail.com>
2025-11-08 17:53:17 +1100

    add apigee proxy svc acct

--- infra/project.tf
+++ infra/project.tf
@@ -28,3 +28,8 @@ resource "google_project_service" "servicenetworking" {
   service            = "servicenetworking.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "iam" {
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
